fe62de83713099ee84641be52472bb00430ef1d7
Vanchenko <vanchenkoev@pakline.com.ua>
2023-04-07 23:48:12 +0300

    new update

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,6 @@ import '../css/styles.css';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from "./fetchCountries";
 
-const elem = document.querySelector('link');
-console.log(elem);
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
 
