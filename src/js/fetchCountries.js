63c610f798681542a66d065d2841ebc87a37e79e
Vanchenko <vanchenkoev@pakline.com.ua>
2023-04-11 10:42:59 +0300

    ups

--- src/js/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,currencies,population,languages,flags`).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    })
-};
